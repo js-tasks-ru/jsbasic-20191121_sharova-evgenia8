1b5c10b76971a4465b8177a1782ecfb046025f90
EvgeniaSharova <58046505+EvgeniaSharova@users.noreply.github.com>
2019-12-04 15:59:12 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * Power
+ * @param {number} m base
+ * @param {number} n index
+ * @returns {number}
+ */
+function sum1(a, b) {
+  return a + b;
+}
+sum1(1, 1);
+function sum2(c, d) {
+  return c + d;
+}
+sum2(2, 5);
